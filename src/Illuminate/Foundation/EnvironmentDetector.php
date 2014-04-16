8076c7528b4132e25f5489c26f182030a69c3f67
Maxim <Big_Shark@mail.ru>
2014-04-16 18:44:59 +1100

    Added check for cli-server

--- src/Illuminate/Foundation/EnvironmentDetector.php
+++ src/Illuminate/Foundation/EnvironmentDetector.php
@@ -38,6 +38,11 @@ class EnvironmentDetector {
 		{
 			return call_user_func($environments);
 		}
+		
+		if(php_sapi_name() == 'cli-server')
+		{
+			return 'local';
+		}
 
 		foreach ($environments as $environment => $hosts)
 		{
